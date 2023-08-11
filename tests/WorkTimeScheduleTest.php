59b081a359318df5282cdf486ab5e7816b73bada
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2023-08-11 14:11:03 +0300

    Fixed bug with find slot by timestamp if a slot end and a next slot start are equals

--- tests/WorkTimeScheduleTest.php
+++ tests/WorkTimeScheduleTest.php
@@ -372,6 +372,17 @@ class WorkTimeScheduleTest extends TestCase
         $this->assertEquals($expectedStartTime, $slot->getStart());
     }
 
+    public function testFindSlotByTimestampExpectsNextSlotIfSlotStartAndNextSlotEndHaveEqualsTime(): void
+    {
+        $expectedStartTime = '10:00';
+        $expectedEndTime = '12:00';
+        $schedule = WorkTimeGenerator::generateWithCustomSlots();
+        $expectedTime = Carbon::createFromTimeString('2023-07-13 10:00:00', $this->getBaseTimeZone());
+        $slot = $schedule->findSlotByTimeStamp($expectedTime->getTimestamp(), $this->getBaseTimeZone());
+        $this->assertEquals($expectedStartTime, $slot->getStart());
+        $this->assertEquals($expectedEndTime, $slot->getEnd());
+    }
+
     public function testFindSlotByTimestampExpectsNull(): void
     {
         $schedule = WorkTimeGenerator::generateWithCustomSlots();
